8c3377741c69eca0b329ec486cfbf348bfeb7729
Matt Rutledge <mattrutledge@believegroup.com>
2017-02-12 15:58:27 -0700

    Add some server logic that still needs to be tested.

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,125 @@
-var initGame = function() {
-    var cfg = {
-        draggable: true,
-        position: 'start',
-        onDrop: handleMove,
-    };
+// setup my socket client
+var socket = io();
+var board, game;
 
-    board = new ChessBoard('gameBoard', cfg);
+// default board config
+var cfg = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onPieceDragStart,
+    onDrop: onPieceDrop,
+    onSnapEnd: onPieceSnapEnd
+};
+
+function InitGame(boardId, config) {
+    if (config === undefined)
+        config = cfg;
+
+    if (boardId === undefined)
+        boardId = 'board';
+
+    board = ChessBoard(boardId, config);
     game = new Chess();
 }
 
-var handleMove = function(source, target) {
-    var move = game.move({ from: source, to: target });
-}
+// do not pick up pieces if the game is over
+// only pick up pieces for the side to move
+function onPieceDragStart(source, piece, position, orientation) {
+    if (game.game_over() === true ||
+        (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+    }
+};
 
-initGame();
+function onPieceDrop(source, target) {
+    // see if the move is legal
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) {
+        return 'snapback';
+    } else {
+        socket.emit('move', {
+            move: move,
+            board: game.fen()
+        });
+    }
+};
+
+// update the board position after the piece snap 
+// for castling, en passant, pawn promotion
+function onPieceSnapEnd() {
+    board.position(game.fen());
+};
+
+/**
+ * Module for displaying "Waiting for..." dialog using Bootstrap
+ *
+ */
+
+var waitingDialog = waitingDialog || (function($) {
+    'use strict';
+
+    // Creating modal dialog's DOM
+    var $dialog = $(
+        '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+        '<div class="modal-dialog modal-m">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
+        '<div class="modal-body">' +
+        '<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+        '</div>' +
+        '</div></div></div>');
+
+    return {
+        /**
+         * Opens our dialog
+         * @param message Custom message
+         * @param options Custom options:
+         * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
+         * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
+         */
+        show: function(message, options) {
+            // Assigning defaults
+            if (typeof options === 'undefined') {
+                options = {};
+            }
+            if (typeof message === 'undefined') {
+                message = 'Loading';
+            }
+            var settings = $.extend({
+                dialogSize: 'm',
+                progressType: '',
+                onHide: null // This callback runs after the dialog was hidden
+            }, options);
+
+            // Configuring dialog
+            $dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
+            $dialog.find('.progress-bar').attr('class', 'progress-bar');
+            if (settings.progressType) {
+                $dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
+            }
+            $dialog.find('h3').text(message);
+            // Adding callbacks
+            if (typeof settings.onHide === 'function') {
+                $dialog.off('hidden.bs.modal').on('hidden.bs.modal', function(e) {
+                    settings.onHide.call($dialog);
+                });
+            }
+            // Opening dialog
+            $dialog.modal();
+        },
+        /**
+         * Closes dialog
+         */
+        hide: function() {
+            $dialog.modal('hide');
+        }
+    };
+
+})(jQuery);
--- server.js
+++ server.js
@@ -21,16 +21,89 @@ console.log('Server Listening on ' + PORT);
 app.use(express.static(__dirname + '/public'));
 
 //////////////////////////////////////////////////////
-// Store the lines drawn
+// Store the users connected and the rooms
 //////////////////////////////////////////////////////
-var linesDrawn = [];
+var lobbyUsers = {}; // Users waiting on gameState
+var users = {}; // Users on system
+var activeGames = {}; // Active Games on System
 
 //////////////////////////////////////////////////////
 // Hook up events for new connections that come in to the server
 //////////////////////////////////////////////////////
 // Called when the client calls socket.emit('move')
 io.on('connection', function(socket) {
-    console.log('Client Connected: ' + socket.id);
+    console.log('New connection ' + socket);
+
+    socket.on('login', function(userId) {
+        console.log(userId + ' joining lobby');
+        socket.userId = userId;
+
+        if (!users[userId]) {
+            console.log('creating new user');
+            users[userId] = { userId: socket.userId, games: {} };
+        } else {
+            console.log('user found!');
+            Object.keys(users[userId].games).forEach(function(gameId) {
+                console.log('gameid - ' + gameId);
+            });
+        }
+
+        lobbyUsers[userId] = socket;
+
+        if (lobbyUsers.length >= 2) {
+            var opponentId = lobbyUsers[0].userId;
+            console.log("Creating Game between" + lobbyUsers[userId].id + " and " + lobbyUsers[opponentId].id)
+            var game = {
+                id: activeGames.length + 1,
+                board: null,
+                users: { white: socket.userId, black: opponentId }
+            };
+
+            socket.gameId = game.id;
+            activeGames[game.id] = game;
+
+            users[game.users.white].games[game.id] = game.id;
+            users[game.users.black].games[game.id] = game.id;
+
+            console.log('Starting game: ' + game.id);
+            lobbyUsers[game.users.white].emit('startGame', { game: game, color: 'white' });
+            lobbyUsers[game.users.black].emit('startGame', { game: game, color: 'black' });
+
+            delete lobbyUsers[game.users.white];
+            delete lobbyUsers[game.users.black];
+        } else {
+            socket.broadcast.emit('joinlobby', socket.userId);
+        }
+    });
+
+    socket.on('invite', function(opponentId) {
+        console.log('got an invite from: ' + socket.userId + ' --> ' + opponentId);
+
+        socket.broadcast.emit('leavelobby', socket.userId);
+        socket.broadcast.emit('leavelobby', opponentId);
+
+
+        var game = {
+            id: Math.floor((Math.random() * 100) + 1),
+            board: null,
+            users: { white: socket.userId, black: opponentId }
+        };
+
+        socket.gameId = game.id;
+        activeGames[game.id] = game;
+
+        users[game.users.white].games[game.id] = game.id;
+        users[game.users.black].games[game.id] = game.id;
+
+        console.log('starting game: ' + game.id);
+        lobbyUsers[game.users.white].emit('joingame', { game: game, color: 'white' });
+        lobbyUsers[game.users.black].emit('joingame', { game: game, color: 'black' });
+
+        delete lobbyUsers[game.users.white];
+        delete lobbyUsers[game.users.black];
+
+        socket.broadcast.emit('gameadd', { gameId: game.id, gameState: game });
+    });
 
     socket.on('move', function(msg) {
         console.log("Move Made");
