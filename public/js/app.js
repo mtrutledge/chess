654d5245a6891952b60e8bdac8c14615c2b00c7a
Matt Rutledge <mattrutledge@believegroup.com>
2017-02-15 11:07:47 -0700

    Go back to a local only game to prep for the PWA in step 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-// setup my socket client
-var socket = io();
-
 // Variables for the actual game and the board for rendering
 var board, game, playerColor;
 
@@ -18,15 +15,14 @@ $(document).ready(function() {
         username = $('#username').val();
 
         if (username.length > 0) {
-            socket.emit('login', username);
             $("#login").hide();
-            waitingDialog.show("Waiting for Opponent");
+            $("#game").show();
+
+            InitGame('board', cfg);
         }
     });
 
     $('#btnResign').on('click', function() {
-        socket.emit('resign', { userId: socket.userId, gameId: game.id });
-
         Resign();
     });
 });
@@ -61,32 +57,6 @@ function ShowMoveList(g) {
     }, this);
 }
 
-socket.on('startGame', function(data) {
-    $("#login").hide();
-    $("#game").show();
-
-    playerColor = data.playerColor;
-
-    InitGame('board', $.extend(cfg, { orientation: playerColor }));
-
-    waitingDialog.hide();
-});
-
-// called when the server calls socket.broadcast('move')
-socket.on('move', function(msg) {
-    game.move(msg.move);
-    board.position(game.fen()); // fen is the board layout
-    AddMove(msg.move);
-});
-
-socket.on('resign', function(msg) {
-    Resign();
-});
-
-socket.on('logout', function(msg) {
-    Resign();
-});
-
 function InitGame(boardId, config) {
     if (config === undefined)
         config = cfg;
@@ -103,8 +73,7 @@ function InitGame(boardId, config) {
 function onPieceDragStart(source, piece, position, orientation) {
     if (game.game_over() === true ||
         (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1) ||
-        (game.turn() !== playerColor[0])) {
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
         return false;
     }
 };
@@ -121,10 +90,6 @@ function onPieceDrop(source, target) {
     if (move === null) {
         return 'snapback';
     } else {
-        socket.emit('move', {
-            move: move,
-            board: game.fen()
-        });
         AddMove(move);
     }
 };
@@ -134,70 +99,3 @@ function onPieceDrop(source, target) {
 function onPieceSnapEnd() {
     board.position(game.fen());
 };
-
-/**
- * Module for displaying "Waiting for..." dialog using Bootstrap
- *
- */
-
-var waitingDialog = waitingDialog || (function($) {
-    'use strict';
-
-    // Creating modal dialog's DOM
-    var $dialog = $(
-        '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
-        '<div class="modal-dialog modal-m">' +
-        '<div class="modal-content">' +
-        '<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
-        '<div class="modal-body">' +
-        '<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
-        '</div>' +
-        '</div></div></div>');
-
-    return {
-        /**
-         * Opens our dialog
-         * @param message Custom message
-         * @param options Custom options:
-         * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
-         * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
-         */
-        show: function(message, options) {
-            // Assigning defaults
-            if (typeof options === 'undefined') {
-                options = {};
-            }
-            if (typeof message === 'undefined') {
-                message = 'Loading';
-            }
-            var settings = $.extend({
-                dialogSize: 'm',
-                progressType: '',
-                onHide: null // This callback runs after the dialog was hidden
-            }, options);
-
-            // Configuring dialog
-            $dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
-            $dialog.find('.progress-bar').attr('class', 'progress-bar');
-            if (settings.progressType) {
-                $dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
-            }
-            $dialog.find('h3').text(message);
-            // Adding callbacks
-            if (typeof settings.onHide === 'function') {
-                $dialog.off('hidden.bs.modal').on('hidden.bs.modal', function(e) {
-                    settings.onHide.call($dialog);
-                });
-            }
-            // Opening dialog
-            $dialog.modal();
-        },
-        /**
-         * Closes dialog
-         */
-        hide: function() {
-            $dialog.modal('hide');
-        }
-    };
-
-})(jQuery);
